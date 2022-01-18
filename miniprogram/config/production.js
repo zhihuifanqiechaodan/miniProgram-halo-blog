2ed54c39c6b5e68194851a143c63b8cbf2db82d9
高宇 <673395239@qq.com>
2022-01-18 22:46:56 +0800

    样式变更，https

--- miniprogram/config/production.js
+++ miniprogram/config/production.js
@@ -1,7 +1,7 @@
 // 开发环境
 const env = "production";
 // 根域名
-const baseUrl = "http://81.70.198.15:8090";
+const baseUrl = "https://www.zhihuifanqiechaodan.com";
 // halo博客后台开启api后设置的token
 const apiAccessKey = "admin!QAZ2wsx";
 
